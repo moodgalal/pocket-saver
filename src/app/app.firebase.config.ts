fae586cb770cdd3b937741e7292c18a04627cde3
Mahmoud Galal Ahmed <mahmod.galal187@gmail.com>
2019-04-27 01:41:59 +0200

    Update app.firebase.config.ts

--- src/app/app.firebase.config.ts
+++ src/app/app.firebase.config.ts
@@ -1,10 +1,10 @@
  let config = {
-    apiKey: "AIzaSyCf8bDUIg89ahosqTFTNx9-1pji3mXGlyI",
-    authDomain: "go-green-15c9c.firebaseapp.com",
-    databaseURL: "https://go-green-15c9c.firebaseio.com",
-    projectId: "go-green-15c9c",
-    storageBucket: "go-green-15c9c.appspot.com",
-    messagingSenderId: "703336634416"
+    apiKey: "kEY hERE",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
 };
 
 export const FIREBASE_CONFIG = config;
